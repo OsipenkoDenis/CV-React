a1e6ad0066840d9a0a83458c7f3688aabde2f425
osipenkoDenis <denis.osipenko@inbox.ru>
2021-12-08 22:50:43 +0300

    correct a mistake

--- src/components/contacts/contacts.jsx
+++ src/components/contacts/contacts.jsx
@@ -10,7 +10,6 @@ const Contacts = () => {
 				<li className="contacts-list__item">Email: osipenkodenisv@gmail.com.</li>
 				<li className="contacts-list__item">Tel: +375296722510.</li>
 				<li className="contacts-list__item">City: Minsk.</li>
-				<li className="contacts-list__item">LinkedIn:<a className="contacts-list__link" href="https://www.linkedin.com/in/%D0%B4%D0%B5%D0%BD%D0%B8%D1%81-%D0%BE%D1%81%D0%B8%D0%BF%D0%B5%D0%BD%D0%BA%D0%BE-71649960/9" target="_blank"> Osipenko Denis.</a></li>
 				<li className="contacts-list__item">Git:<a className="contacts-list__link" href="https://github.com/OsipenkoDenis" target="_blank"> Osipenko Denis.</a></li>
 			</ul>
 		</article>
